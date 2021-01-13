fbde9aba3f56ef40409022c8ddc4db715827fc22
racross1 <racross1@gmail.com>
2021-01-12 16:07:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+//Event Listeners
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+  })
+
+
+//Actions  
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 180*2) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  
